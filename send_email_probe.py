08c5b785cad1592ee11ca2038e4c834ec780e74e
Igor Belov <belov.bisgarik@yandex.ru>
2022-09-04 21:08:28 +0300

    rename file

--- send_email_probe.py
+++ send_email_probe.py
@@ -1,41 +1,23 @@
-import pandas as pd
 import smtplib
-import pretty_html_table
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 
-mail_info_1 = []
 
-with open('/Users/bisgarik/PycharmProjects/SmartReport/from_to_emails.txt') as mail_info:
-    for line in mail_info:
-        mail_info_1.append(line[:-1])
-    from_ = mail_info_1[0]
-    to_ = mail_info_1[1]
+def send_email(host, subject, from_addr, to_addr, body):
+    """
+    :param host:
+    :param subject:
+    :param from_addr:
+    :param to_addr:
+    :param body:
+    """
+    # server = smtplib.SMTP(host=host, port=25)
 
-    data = {'test1': {'rootvol': 'Optimal'},
-            'test2': {'rootvol': 'Optimal', 'datavol': 'Optimal'},
-            'test3': {'rootvol': 'Optimal', 'datavol1': 'Optimal', 'datavol2': 'Optimal', 'ssdvol': 'Optimal'}
-            }
+    mail_body = "\r\n".join((
+        "From: %s" % from_addr,
+        "To: %s" % to_addr,
+        "Subject: %s" % subject,
+        "",
+        body
+    ))
 
-    df = pd.DataFrame.from_dict(data=data, orient='index')
-    df.to_html()
-    table = pretty_html_table.build_table(df=df, color='blue_light', index=True)
-
-    # подключаемся к SMTP серверу
-    # server = smtplib.SMTP('mx.prolog.ltd')  # TODO make server getting from file
-
-    # создаём письмо
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = 'Adaptec report'
-    msg['From'] = from_
-    msg['To'] = to_
-
-    #
-    main_table = MIMEText('<h3>Adaptec report</h3>', 'html')
-    table = MIMEText(table, 'html')
-
-    msg.attach(table)
-
-    # server.send_message(msg)
-    print(df)
+    # server.sendmail(from_addr, to_addr, mail_body)
     # server.quit()
