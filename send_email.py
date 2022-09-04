29d613e3572816aadafb6676cb296c27d95f85ee
Igor Belov <belov.bisgarik@yandex.ru>
2022-09-04 21:39:11 +0300

    made mail sender on class

--- send_email.py
+++ send_email.py
@@ -10,21 +10,26 @@ FROM_TO_EMAILS = '/Users/bisgarik/PycharmProjects/SmartReport/from_to_emails.txt
 class SmartReportSender:
 
     def __init__(self, report_result):
+        self.report_result = report_result
+        self.mailserver_info_path = ''
+        self.message = None
+        self.table = None
+        self.main_table = None
+        self.msg = None
         self.server = None
         self.df = None
         self.to_ = ''
         self.from_ = ''
-        self.mailserver_info = []
-        self.report_result = report_result
-        self.mail_info_1 = []
+        self.mailserver_info = None
+        self.mailserver_info_list = []
 
-    def get_mailserver_info(self, mailserver_info):
-        self.mailserver_info = mailserver_info
-        with open(FROM_TO_EMAILS) as self.mailserver_info:
+    def get_mailserver_info(self, mailserver_info_path):
+        self.mailserver_info_path = mailserver_info_path
+        with open(self.mailserver_info_path) as self.mailserver_info:
             for line in self.mailserver_info:
-                self.mailserver_info.append(line[:-1])
-            self.from_ = self.mailserver_info[0]
-            self.to_ = self.mailserver_info[1]
+                self.mailserver_info_list.append(line[:-1])
+            self.from_ = self.mailserver_info_list[0]
+            self.to_ = self.mailserver_info_list[1]
 
     def get_data_frame(self):
         self.df = pd.DataFrame.from_dict(data=self.report_result, orient='index')
@@ -39,43 +44,33 @@ class SmartReportSender:
         self.server = smtplib.SMTP('mx.prolog.ltd')  # TODO make server getting from file
 
     def create_message(self):
-        pass
+        self.msg = MIMEMultipart('alternative')
+        self.msg['Subject'] = 'Adaptec report'
+        self.msg['From'] = self.from_
+        self.msg['To'] = self.to_
+        self.main_table = MIMEText('<h3>Adaptec report</h3>', 'html')
+        self.table = MIMEText(self.table, 'html')
+        self.msg.attach(self.table)
+
+    def send_message(self, message):
+        self.message = message
+        self.server.send_message(message=self.msg)
 
     def run(self):
+        self.get_mailserver_info(mailserver_info_path=FROM_TO_EMAILS)
+        self.get_data_frame()
+        # self.smtp_connect()
+        self.create_message()
         # server.send_message(msg)
-        # print(df)
+        print(self.df)
         # server.quit()
-        pass
 
 
-
-
-
-
+if __name__ == '__main__':
     data = {'test1': {'rootvol': 'Optimal'},
             'test2': {'rootvol': 'Optimal', 'datavol': 'Optimal'},
             'test3': {'rootvol': 'Optimal', 'datavol1': 'Optimal', 'datavol2': 'Optimal', 'ssdvol': 'Optimal'}
             }
 
-    # df = pd.DataFrame.from_dict(data=data, orient='index')
-    # df.to_html()
-    # table = pretty_html_table.build_table(df=df, color='blue_light', index=True)
-
-    # подключаемся к SMTP серверу
-    # server = smtplib.SMTP('mx.prolog.ltd')  # TODO make server getting from file
-
-    # создаём письмо
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = 'Adaptec report'
-    msg['From'] = from_
-    msg['To'] = to_
-
-    #
-    main_table = MIMEText('<h3>Adaptec report</h3>', 'html')
-    table = MIMEText(table, 'html')
-
-    msg.attach(table)
-
-    # server.send_message(msg)
-    print(df)
-    # server.quit()
+    report_message = SmartReportSender(report_result=data)
+    report_message.run()
