b3fa4413e736a22d1f5ed07bc3455ff23cf9f6d8
Igor Belov <belov.bisgarik@yandex.ru>
2022-10-05 20:56:54 +0500

    убрал формирование дата фрейма

--- send_email.py
+++ send_email.py
@@ -1,41 +1,27 @@
 # -*- coding: utf-8 -*-
 
-import pandas as pd
 import smtplib
-import pretty_html_table
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email_server import SMTP_SERVER, FROM, TO
 
 
-class SmartReportSender:
+class ReportSender:
 
-    def __init__(self, report_result):
-        self.report_result = report_result
-        # self.mailserver_info_path = ''
+    def __init__(self, subject, body):
+        self.body = body
+        self.subject = subject
         self.message = None
-        self.table = None
+        self.body = None
         self.main_table = None
         self.msg = None
         self.server = None
         self.df = None
-        # self.to_ = ''
-        # self.from_ = ''
-        # self.mailserver_info = None
-        # self.mailserver_info_list = []
 
-    # def get_mailserver_info(self, mailserver_info_path):
-    #     self.mailserver_info_path = mailserver_info_path
-    #     with open(self.mailserver_info_path) as self.mailserver_info:
-    #         for line in self.mailserver_info:
-    #             self.mailserver_info_list.append(line[:-1])
-    #         self.from_ = self.mailserver_info_list[0]
-    #         self.to_ = self.mailserver_info_list[1]
-
-    def get_data_frame(self):
-        self.df = pd.DataFrame.from_dict(data=self.report_result, orient='index')
-        self.df.to_html()
-        self.table = pretty_html_table.build_table(df=self.df, color='blue_light', index=True)
+    # def get_data_frame(self):
+    #     self.df = pd.DataFrame.from_dict(data=self.report_result, orient='index')
+    #     self.df.to_html()
+    #     self.table = pretty_html_table.build_table(df=self.df, color='blue_light', index=True)
 
     def smtp_connect(self):
         """
@@ -46,22 +32,19 @@ class SmartReportSender:
 
     def create_message(self):
         self.msg = MIMEMultipart('alternative')
-        self.msg['Subject'] = 'Adaptec report'
+        self.msg['Subject'] = self.subject
         self.msg['From'] = FROM
         self.msg['To'] = TO
-        self.main_table = MIMEText('<h3>Adaptec report</h3>', 'html')
-        self.table = MIMEText(self.table, 'html')
-        self.msg.attach(self.table)
+        self.body = MIMEText(self.body, 'html')
+        self.msg.attach(self.body)
 
     def send_message(self, message):
         self.message = message
         self.server.send_message(msg=self.msg)
 
     def run(self):
-        # self.get_mailserver_info(mailserver_info_path=FROM_TO_EMAILS)
-        self.get_data_frame()
+        # self.get_data_frame()
         self.smtp_connect()
-        print(self.df)
         self.create_message()
         self.send_message(self.msg)
         self.server.quit()
@@ -73,5 +56,6 @@ if __name__ == '__main__':
             'test3': {'rootvol': 'Optimal', 'datavol1': 'Optimal', 'datavol2': 'Optimal', 'ssdvol': 'Optimal'}
             }
 
-    report_message = SmartReportSender(report_result=data)
+    report_message = ReportSender(subject='Adaptec report',
+                                  body=data)
     report_message.run()
