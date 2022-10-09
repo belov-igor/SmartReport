9d916f7ba453c28f50c40eef1f4d9d7e34edce90
Igor Belov <belov.bisgarik@yandex.ru>
2022-10-10 00:32:48 +0600

    добавил info

--- send_email.py
+++ send_email.py
@@ -3,7 +3,7 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email_server import SMTP_SERVER, FROM, TO
+from info.email_server import SMTP_SERVER, FROM, TO
 
 
 class ReportSender:
