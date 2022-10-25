a4c2df66a13461b6f9eaa72c18a63265649df86c
belov-igor <bisgarik@gmail.com>
2022-10-25 11:28:08 +0500

    разбиение на windows и linux хосты + опрос esxi

--- get_adaptec_report.py
+++ get_adaptec_report.py
@@ -4,15 +4,16 @@ import subprocess
 import pandas as pd
 import pretty_html_table
 
-from info.hosts import HOSTS
+from info.hosts import WINDOWS_HOSTS, LINUX_HOSTS
 from send_email import ReportSender
 
 
 class SmartReport:
 
-    def __init__(self, hostname):
+    def __init__(self, username, hostname):
         self.df = None
         self.table = None
+        self.username = username
         self.hostname = hostname
         self.config = dict()
         self.device_name = ''
@@ -20,8 +21,11 @@ class SmartReport:
         self.data = ''
 
     def logical_device_status(self):
+        arcconf_path = 'arcconf'
+        if 'esxi' in self.hostname:
+            arcconf_path = f'/vmfs/volumes/{self.hostname}_datavol/arcconf'
         connect = subprocess.run(
-            ["ssh", f"root@{self.hostname}", "/root/bin/arcconf", "GETCONFIG", "1", "ld"],
+            ["ssh", f"{self.username}@{self.hostname}", f"{arcconf_path}", "GETCONFIG", "1", "ld"],
             stdout=subprocess.PIPE)
         self.data = connect.stdout.decode().split('\n')
         for string in self.data:
@@ -29,7 +33,7 @@ class SmartReport:
                 self.device_name = string.split()[-1]
             if 'status of logical device' in string.lower():
                 self.device_status = string.split()[-1]
-        self.config = {self.device_name: self.device_status}
+            self.config.update({self.device_name: self.device_status})
 
     def run(self):
         self.logical_device_status()
@@ -39,26 +43,34 @@ class SmartReport:
 def get_data_frame(data):
     df = pd.DataFrame.from_dict(data=data, orient='index')
     df.to_html()
-    table = pretty_html_table.build_table(df=df, color='blue_light', index=True)
+    table = pretty_html_table.build_table(df=df, color='blue_light', index=True,
+                                          text_align='center', padding="0px 5px 0px 5px")
     return table
 
 
 if __name__ == '__main__':
 
-    with open('info/hosts.py') as hosts:
-        adaptec_report = dict()
+    adaptec_report = dict()
+    hosts = dict()
 
-        try:
-            for host in HOSTS:
-                report = SmartReport(hostname=host)
+    try:
+        #
+        windows_hosts = [host for host in WINDOWS_HOSTS]
+        linux_hosts = [host for host in LINUX_HOSTS]
+        hosts.update({'root': linux_hosts,
+                      'Administrator': windows_hosts})
+
+        for user_name, host in hosts.items():
+            for hostname in host:
+                report = SmartReport(username=user_name, hostname=hostname)
                 ld_stat = report.run()
-                adaptec_report.update({host: ld_stat})
-            report_table = f'<h3>Adaptec report</h3> \n ' \
-                           f'{get_data_frame(data=adaptec_report)} '
-        except Exception as error:
-            report_table = error
-        else:
-            # print(report_table)
-            report_message = ReportSender(subject='Adaptec report',
-                                          body=report_table)
-            report_message.run()
+                adaptec_report.update({hostname: ld_stat})
+        report_table = f'<h3>Adaptec report</h3>\n'\
+                       f'{get_data_frame(data=adaptec_report)}'
+    except Exception as error:
+        report_table = error
+    else:
+        # print(report_table)
+        report_message = ReportSender(subject='Adaptec report',
+                                      body=report_table)
+        report_message.run()
