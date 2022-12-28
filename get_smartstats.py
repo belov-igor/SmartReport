2bcbdf8502cc07b2d52e7d03691018c84a76ded5
belov-igor <bisgarik@gmail.com>
2022-12-28 13:37:34 +0500

    smartreport v1.0

--- get_smartstats.py
+++ get_smartstats.py
@@ -1,37 +1,119 @@
 # -*- coding: utf-8 -*-
 
-import subprocess
-import xml.etree.ElementTree as ETree
-import pandas as pd
-import pretty_html_table
 import copy
+import subprocess
+import os
+import xml.etree.ElementTree as ETree
 
-from info.hosts import WINDOWS_HOSTS, LINUX_HOSTS  # хосты из файла, разделены на списки windows и linux
+import numpy
+import pandas as pd
+from datetime import date
+
+from config.hosts import SMARTCTL_HOSTS, ADAPTEC_HOSTS
 from send_email import ReportSender
 
-PARAMS = {
-    '0x09': 'Power-On Hours',
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DRIVE_COUNT_TMP_PATH = os.path.join(BASE_DIR, 'tmp/drive_count_tmp.txt')
+DRIVE_COUNT_TMP_NEW_PATH = os.path.join(BASE_DIR, 'tmp/drive_count_tmp.txt')
+
+DISC_PARAMETERS = {
     '0x05': 'Reallocated Sectors Count',
+    '0x09': 'Power-On Hours',
     '0xC5': 'Current Pending Sector Count',
     '0xC6': 'Uncorrectable Sectors Count'
 }
 
+TABLE_STYLES = [
+    dict(selector="tr:hover",
+         props=[
+             ("background", "#f4f4f4")]),
+    dict(selector="th",
+         props=[
+             ("color", "black"),
+             ("border", "0 solid #305496"),
+             ("padding", "0 5px"),
+             ("font-family", 'sans-serif'),
+             ("border-collapse", "collapse"),
+             ("background", "#D9E1F2"),
+             ("font-size", "14px")
+         ]),
+    dict(selector="td",
+         props=[
+             ("color", "black"),
+             ("font-family", 'sans-serif'),
+             ("border", "0.5px solid #eee"),
+             ("padding", "0 0"),
+             ("text-align", "center"),
+             ("border-collapse", "collapse"),
+             ("font-size", "14px")
+         ]),
+]
 
-class SmartReport:
+
+class SmartCtlReport:
 
     def __init__(self, username, hostname):
-        self.config1 = {}
-        self.drives = []
-        self.df = None
-        self.table = None
         self.username = username
         self.hostname = hostname
-        self.config = dict()
+        self.report = dict()
+        self.drives_count = 0
+        self.smartctl_script_result = None
+
+    def get_smartctl_data(self):
+        """
+        :return
+        """
+        open_script = subprocess.Popen(['cat /root/bin/smartctl_script.sh'], stdout=subprocess.PIPE, shell=True)
+        ssh_connect = subprocess.Popen([f'ssh {self.username}@{self.hostname}'], stdin=open_script.stdout,
+                                       stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        open_script.stdout.close()
+        self.smartctl_script_result = ssh_connect.communicate()[0].decode().split('\n')
+
+    def get_smartctl_report(self):
+        """
+
+        :return:
+        """
+        power_on_hours, reallocated_sector_ct, current_pending, offline_uncorrectable = '-', '-', '-', '-'
+        sg_name = ''
+        sg_list = []
+        for string in self.smartctl_script_result:
+            if "/dev/" in string.lower():
+                sg_name = string[5:]
+                sg_list.append(sg_name)
+                self.drives_count = len(sg_list)
+            if 'reallocated' in string.lower():
+                reallocated_sector_ct = int(string.split()[-1])
+            if 'power_on_hours' in string.lower():
+                power_on_hours = int(string.split()[9])
+            if 'current_pending' in string.lower():
+                current_pending = int(string.split()[-1])
+            if 'offline_uncorrectable' in string.lower():
+                offline_uncorrectable = int(string.split()[-1])
+            self.report.update({sg_name: {'Reallocated Sectors Count': reallocated_sector_ct,
+                                          'Power-On Hours': power_on_hours,
+                                          'Current Pending Sector Count': current_pending,
+                                          'Uncorrectable Sectors Count': offline_uncorrectable}})
+
+    def run(self):
+        #
+        self.get_smartctl_data()
+        self.get_smartctl_report()
+
+        return self.drives_count, self.report
+
+
+class AdaptecSmartReport:
+
+    def __init__(self, username, hostname):
+        self.username = username
+        self.hostname = hostname
+        self.report = dict()
+        self.drives_count = 0
         self.min_string = 0
         self.max_string = 0
-        # self.device_name = ''
-        # self.device_status = ''
-        self.smarts_data = ''
+        self.adaptec_smart_data = None
+        self.config1 = {}  # TODO rename
 
     def get_adaptec_smarts_data(self):
         """
@@ -49,68 +131,62 @@ class SmartReport:
         connect = subprocess.run(
             ["ssh", f"{self.username}@{self.hostname}", f"{arcconf_path}", "GETSMARTSTATS", "1"],
             stdout=subprocess.PIPE)
-        self.smarts_data = connect.stdout.decode().split('\n')
-        for string in self.smarts_data:
+        self.adaptec_smart_data = connect.stdout.decode().split('\n')
+        for string in self.adaptec_smart_data:
             if '<SmartStats' in string:
-                self.min_string = self.smarts_data.index(string)
+                self.min_string = self.adaptec_smart_data.index(string)
             if '</SmartStats>' in string:
-                self.max_string = self.smarts_data.index(string) + 1
+                self.max_string = self.adaptec_smart_data.index(string) + 1
         with open('/tmp/smartstats_temp.xml', 'w') as smartstats_xml:
-            xml = '\n'.join(self.smarts_data[self.min_string:self.max_string]) + '\n'
+            xml = '\n'.join(self.adaptec_smart_data[self.min_string:self.max_string]) + '\n'
             smartstats_xml.write(xml)
-        # smartstats_xml.close()
 
     def get_adaptec_smart_report(self):
-        self.drives = []
-        stats = ETree.parse('/tmp/smartstats_temp.xml')
-        # Найти корень
-        root = stats.getroot()
-        for root_attrib in root.attrib:
-            if 'devicename' in root_attrib.lower():
-                device_name = root.attrib[root_attrib]
-        for physical_drives in root:
-            drive = physical_drives.attrib['id']
-            self.drives.append(physical_drives.attrib['id'])
-            for drives_attrib in physical_drives:
-                param_id = drives_attrib.attrib['id']
-                if param_id in PARAMS:
-                    # print(param_id)
-                    param_name = PARAMS[param_id]
-                    param_value = int(drives_attrib.attrib["rawValue"])
-                    self.config1.update({param_name: param_value})
-            self.config.update({f'id={drive}': copy.deepcopy(self.config1)})
-
-    def get_disks(self):
-        """
-        :return
-        """
-        pass
-
-    def smartctl_report(self):
         """
 
         :return:
         """
-        # Подключение к хостам по ssh, получение данных smartctl
-        # connect = subprocess.run(
-        #     ["ssh", f"{self.username}@{self.hostname}", f"{arcconf_path}", "GETSMARTSTATS", "1"],
-        #     stdout=subprocess.PIPE)
-        # # self.smarts_data = connect.stdout.decode().split('\n')
-        # with open('smartctl_out.txt', 'r') as stats:
-        #     for string in stats:
-        #         if 'power_on_hours' in string.lower():
-        #             power_on_hours = string.split()[-1]
-        #         if 'reallocated' in string.lower():
-        #             reallocated_sector_ct = string.split()[-1]
-        #         if 'current_pending' in string.lower():
-        #             current_pending = string.split()[-1]
-        #         if 'offline_uncorrectable' in string.lower():
-        #             offline_uncorrectable = string.split()[-1]
+        drives = []
+        stats = ETree.parse('/tmp/smartstats_temp.xml')
+        # Найти корень
+        root = stats.getroot()
+        for physical_drives in root:
+            drive = physical_drives.attrib['id']
+            drives.append(physical_drives.attrib['id'])
+            self.config1 = {}
+            for drives_attrib in physical_drives:
+                param_id = drives_attrib.attrib['id']
+                if param_id in DISC_PARAMETERS:
+                    param_name = DISC_PARAMETERS[param_id]
+                    param_value = drives_attrib.attrib["rawValue"]
+                    self.config1.update({param_name: param_value})
+                self.report.update({f'id={drive}': copy.deepcopy(self.config1)})
+            self.drives_count = len(drives)
 
     def run(self):
-        # self.get_adaptec_smarts_data()
+
+        #
+        self.get_adaptec_smarts_data()
         self.get_adaptec_smart_report()
-        return self.config
+        #
+        os.remove('/tmp/smartstats_temp.xml')
+
+        return self.drives_count, self.report
+
+
+def drive_count(host, drive_count):
+    old_date = ''
+    disc_count_old = ''
+    with open(DRIVE_COUNT_TMP_PATH, 'r') as drive_count_log_old:
+        for line in drive_count_log_old:
+            if host in line.split():
+                old_date = line.split()[1]
+                disc_count_old = line.split()[2]
+    with open(DRIVE_COUNT_TMP_NEW_PATH, 'a') as drive_count_log_new:
+        new_date = date.today().strftime("%d.%m.%Y")
+        drive_count_log_new.write(f'{host} {new_date} {drive_count}\n')
+    return f'Всего дисков на сегодня ({new_date}) - <b>{drive_count}</b>. ' \
+           f'Дисков при прошлой проверке ({old_date}) - <b>{disc_count_old}</b>.'
 
 
 def get_data_frame(data):
@@ -119,32 +195,17 @@ def get_data_frame(data):
     :param data: данные в виде словаря
     :return: данные, сформированные в html-таблицу
     """
-    styles = [
-        dict(selector="tr:hover",
-             props=[("background", "#f4f4f4")]),
-        dict(selector="th", props=[("color", "black"),
-                                   ("border", "0 solid #305496"),
-                                   ("padding", "0 5px"),
-                                   ("font-family", 'sans-serif'),
-                                   ("border-collapse", "collapse"),
-                                   ("background", "#D9E1F2"),
-                                   ("font-size", "14px")
-                                   ]),
-        dict(selector="td", props=[("color", "black"),
-                                   ("font-family", 'sans-serif'),
-                                   ("border", "0.5px solid #eee"),
-                                   ("padding", "0px 0px"),
-                                   ("text-align", "center"),
-                                   ("border-collapse", "collapse"),
-                                   ("font-size", "14px")
-                                   ]),
-    ]
     df = pd.DataFrame.from_dict(data=data, orient='index')
-
-    table = df.style.set_table_styles(table_styles=styles).format(
-        {'Reallocated Sectors Count': lambda x: f'<p style="background-color:red">{x}</p>' if int(x) > 0 else x},
-        {'Current Pending Sector Count': lambda x: f'<p style="background-color:red">{x}</p>' if int(x) > 0 else x},
-        {'Uncorrectable Sectors Count': lambda x: f'<p style="background-color:red">{x}</p>' if int(x) > 0 else x}
+    df = df.replace({numpy.nan: '-'})
+    table = df.style.set_table_styles(table_styles=TABLE_STYLES). \
+        format(
+        {'Reallocated Sectors Count':
+            lambda x: f'<p style="background-color:red">{x}</p>' if x != '-' and int(x) > 0 else x,
+         'Current Pending Sector Count':
+            lambda x: f'<p style="background-color:red">{x}</p>' if x != '-' and int(x) > 0 else x,
+         'Uncorrectable Sectors Count':
+            lambda x: f'<p style="background-color:red">{x}</p>' if x != '-' and int(x) > 0 else x
+         },
     ).render()
 
     return table
@@ -152,32 +213,35 @@ def get_data_frame(data):
 
 if __name__ == '__main__':
 
-    adaptec_report = dict()
-    hosts_dict = dict()
+    host = str()
 
     try:
-        # Формирование словаря со списком хостов, где ключ - имя пользователя
-        hosts_dict.update({'root': LINUX_HOSTS,
-                           'Administrator': WINDOWS_HOSTS})
 
         report_table = '<h2>SMART report</h2>\n'
 
         # Проход по хостам и формирование отчета в виде html-таблицы
-        for user_name, hosts in hosts_dict.items():
-            for host in hosts:
-                report = SmartReport(username=user_name, hostname=host)
-                ld_stat = report.run()
-                adaptec_report.update({host: ld_stat})
-                report_table = report_table + f'<h3>{host}</h3>\n' \
-                                              f'{get_data_frame(data=ld_stat)}'
+        for host, user_name in SMARTCTL_HOSTS.items():
+            smartctl_report = SmartCtlReport(username=user_name, hostname=host)
+            disc_count, report = smartctl_report.run()
+            report_table = report_table + f'<h3>{host}</h3>\n' \
+                                          f'{drive_count(host=host, drive_count=disc_count)}' \
+                                          f'{get_data_frame(data=report)}'
+
+        for host, user_name in ADAPTEC_HOSTS.items():
+            smart_adaptec_report = AdaptecSmartReport(username=user_name, hostname=host)
+            disc_count, report = smart_adaptec_report.run()
+            report_table = report_table + f'<h3>{host}</h3>\n' \
+                                          f'{drive_count(host=host, drive_count=disc_count)}' \
+                                          f'{get_data_frame(data=report)}'
+
     except Exception as error:
         report_table = error
-        print(error)
+        print(f'{host}: {error}')
     else:
+        os.rename(DRIVE_COUNT_TMP_NEW_PATH, DRIVE_COUNT_TMP_PATH)
+
         # Отправка отчета
         # TODO need done sending error with mail
-        with open('test.html', 'w') as html:
-            html.write(report_table)
-    #     report_message = ReportSender(subject='SMART report',
-    #                                   body=report_table)
-    #     report_message.run()
+        report_message = ReportSender(subject='SMART report',
+                                      body=report_table)
+        report_message.run()
